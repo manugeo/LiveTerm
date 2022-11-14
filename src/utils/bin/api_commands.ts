94e40d8a006de8bd1d7d32fe3b4bb4614adb7b9b
Manu <manugeo13@gmail.com>
2022-11-14 20:44:12 +0530

    Updated site title and default weather helper text.

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -29,7 +29,7 @@ export const readme = async (args: string[]): Promise<string> => {
 export const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Usage: weather [city]. Example: weather Dubai';
   }
   const weather = await getWeather(city);
   return weather;
